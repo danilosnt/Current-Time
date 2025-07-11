bb420a203157e3d6889db8795f241ced94bb77cc
Danilo Santos <daniloddocarmo16@icloud.com>
2025-07-10 23:41:10 -0300

    subindo componente

---
+++ src/components/ComponenteFuncional.jsx
@@ -0,0 +1,10 @@
+function ComponenteFuncional() {
+  return (
+    <div>
+      <h1></h1>
+      <p>Este es un componente funcional en React.</p>
+    </div>
+  );
+}
+
+export default ComponenteFuncional;
