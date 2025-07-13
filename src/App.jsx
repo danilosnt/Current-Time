0d8a21f2d7b5b11f34260f78778a525981cda91f
Danilo Santos <daniloddocarmo16@icloud.com>
2025-07-13 19:42:01 -0300

    ultimo teste de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ComponenteFuncional from './components/ComponenteFuncional'
-import PropsExample from './components/PropsExample'
+import JsxExemplos from './components/JsxExemplos'
 
 function App() {
 
   return (
     <>
       <h1>Hello World</h1>
-      <ComponenteFuncional />
-      <PropsExample nome="Danilo" tempo={20} />
+      <JsxExemplos />
     </>
   )
 }
